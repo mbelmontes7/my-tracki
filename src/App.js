9811aa5822ea5970fbdfd04df3da384646f36381
mbelmontes7 <you@example.com>
2023-01-18 20:05:39 -0600

    created the first simple page/ working  rendering

--- src/App.js
+++ src/App.js
@@ -3,27 +3,41 @@ import './App.css';
 
 function App() {
   return (
-   
-<div class="bg-white dark:bg-gray-800 ">
-    <div class="text-m-10">
-        <h2 class="text-10xl font-extrabold text-black dark:text-white sm:text-4xl">
-            <span class="block text-center">
-               Welcome to tracki
-            
-            </span>
-        </h2>
-        <div class="lg:mt-0 lg:flex-shrink-0">
-            <div class="mt-12 inline-flex rounded-md shadow">
-                <button type="button" class="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                    Get started
-                </button>
-            </div>
-        </div>
-    </div>
-</div>
+
+
+    <html> 
+        <head> 
+            <link href= 
+    "https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+                rel="stylesheet" /> 
+        </head> 
+        <body> 
+            <div class="h-screen flex flex-col 
+                        items-center justify-center"> 
+                <p class="text-black-900 text-5xl mb-3"> 
+                Welcome to tracki
+                </p> 
+                <form> 
+                    <input aria-label="Enter your email address"
+                        type="text" placeholder="Email address"
+                        class="text-sm text-gray-base w-full 
+                                mr-3 py-5 px-4 h-2 border 
+                                border-black-900 rounded mb-2" /> 
+                    <input aria-label="Enter your password"
+                        type="password" placeholder="Password"
+                        class="text-sm text-gray-base w-full mr-3 
+                                py-5 px-4 h-2 border border-black-200 
+                                rounded mb-2" /> 
+    
+                    <button type="submit"
+                            class="bg-green-400 w-full mt-4"> 
+                        Login 
+                    </button> 
+                </form> 
+            </div> 
+        </body> 
+    </html> 
 
   )
 }
-
-
 export default App;
